a87b5be5e80b5159fa9221b112fb63aa5c07d49b
hanmacrad2 <74420902+hanmacrad2@users.noreply.github.com>
2022-08-16 17:07:31 -0400

    Model Comparison apply
    Model Comparison apply

--- Run/Apply_Model_Comparison.R
+++ Run/Apply_Model_Comparison.R
@@ -1,10 +1,11 @@
 #Apply Model Comparison
 
-#1. RUN SSE MODEL
-mcmc_sse_output = SSE_MCMC_ADAPTIVE(canadaX)
-
-#2. RUN SSI MODEL
+#1. RUN SSI MODEL
 mcmc_ssi_output = SSI_MCMC_ADAPTIVE(sim_data_canadaX1)
+
+#2. RUN SSE MODEL
+mcmc_sse_output = SSE_MCMC_ADAPTIVE(canadaX)
 
 #3. GET HARMONIC MEANS
 bf = get_bayes_factor_harmonic_means(mcmc_ssi_output$log_like_vec, mcmc_sse_output$log_like_vec)
+print(paste0('bayes factor = ', bf))
