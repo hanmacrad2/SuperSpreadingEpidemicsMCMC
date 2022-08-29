a54649bff44a154481b04b487bb355d90f92e0b7
hanmacrad2 <74420902+hanmacrad2@users.noreply.github.com>
2022-08-29 11:22:09 -0400

    Model Criticism
    Model Criticism

--- R/MODEL_CRITICISM.R
+++ R/MODEL_CRITICISM.R
@@ -33,7 +33,7 @@ GET_P_VALUE <- function(column_true_val, column_summary_stat) {
 #' Returns the summary statistics of the data in a dataframe or list format
 #' 
 #' @param data Data for which the summary statistics are calculated
-#' @param FLAG_MAKE_DF Flag to indiciate whether to return a dataframe or a list of summary statistics 
+#' @param FLAG_MAKE_DF Flag to indicate whether to return a dataframe or a list of summary statistics 
 #' @return summary statistics of the data in a dataframe or list format 
 #' @export
 #'
@@ -45,17 +45,17 @@ GET_P_VALUE <- function(column_true_val, column_summary_stat) {
 #' 
 GET_SUMMARY_STATS <- function(data, FLAG_MAKE_DF){
 
-  start_half2 = (length(data)/2)+1
-  stop_half2 = length(data)
+  #Initialise
+  mid_point = length(data)/2; end = length(data)
   
   if (FLAG_MAKE_DF){
     
-    #Df of summary stats (20)
+    #Data-frame of summary statistics (20)
     summary_stats_results = data.frame( 
       
       sum_infects = sum(data),
-      sum_1st_half  = sum(data[1:(length(data)/2)]),
-      sum_2nd_half =  sum(data[start_half2:stop_half2]),
+      sum_1st_half  = sum(data[1:mid_point]),
+      sum_2nd_half =  sum(data[mid_point+1:end]),
       
       median_infect = median(data),
       max_infect = max(data),
@@ -87,7 +87,7 @@ GET_SUMMARY_STATS <- function(data, FLAG_MAKE_DF){
   } else {
     
     #List if Dataframe already created
-    summary_stats_results = list(sum(data), sum(data[1:(length(data)/2)]), sum(data[start_half2:stop_half2]),
+    summary_stats_results = list(sum(data), sum(data[1:mid_point]), sum(data[mid_point +1:end]),
                                  median(data), max(data), sd(data),
                                  quantile(data)[4][1][1], quantile(data, probs = seq(0, 1, 0.125))[8][1][1],
                                  max(diff(data)), median(diff(data)), max(diff(diff(data))), median(diff(diff(data))),
@@ -105,9 +105,134 @@ GET_SUMMARY_STATS <- function(data, FLAG_MAKE_DF){
 }
 
 ####################
-RUN_MCMC_REPS(N_REPS){}
+RUN_MCMC_REPS <- function(epidemic_data, root_folder,
+                          model_type = list(FLAG_BASE = TRUE, FLAG_SSE = FALSE, FLAG_SSI = FALSE),
+                          modelling_specs = list(n_reps = 500, n_mcmc = 500000)){
+  
+  'Run mcmc for n_reps iterations and save' 
+  
+  for(rep in 1:modelling_specs$n_reps) {
+    
+    #Folder 
+    cat('\n rep =', rep, '\n')
+    folder_rep = paste0(base_folder, '/rep_', rep)
+    ifelse(!dir.exists(file.path(folder_rep)), dir.create(file.path(folder_rep), recursive = TRUE), FALSE)
+    
+    #MCMC 
+    if (model_type$FLAG_BASE){
+      
+      mcmc_output = 
+    } else if (model_type$FLAG_SSE) {
+      
+      mcmc_output = SSE_MCMC_ADAPTIVE(epidemic_data)
+      
+    } else if (model_type$FLAG_SSI) {
+      
+      #Data (Fix to SSI?)
+      mcmc_output = SSI_MCMC_ADAPTIVE(epidemic_data)
+    }
+    
+    mcmc_params = mcmc_sse(sim_data, n, sigma, thinning_factor, folder_rep, rep, burn_in)
+    
+    #SAVE MCMC PARAMS 
+    saveRDS(mcmc_params, file = paste0(folder_rep, '/mcmc_params_rep_', rep, '.rds' ))
+  }
+  
+  
+}
+
+run_mcmc_reps_ss <- function(n, n_reps, model_params, sigma, flag_dt, base_folder, burn_in){
+  
+  'Run mcmc for n_reps iterations and save'
+  
+  #Get model parameters
+  alphaX = model_params[1]; betaX = model_params[2]
+  gammaX = model_params[3]; r0 = model_params[4];
+  
+  #Data_type
+  flag1 = flag_dt[1]; flag2 = flag_dt[2]; flag3 = flag_dt[3] 
+  start_rep = 1
+  cat('r0 = ', r0, '\n');
+  
+  # if (flag1){
+  #   start_rep = 462
+  # } else {
+  #   start_rep = 1
+  # }
+  
+  #Repeat for n reps
+  for(rep in start_rep:n_reps) {
+    
+    cat('\n rep =', rep, '\n')
+    folder_rep = paste0(base_folder, '/rep_', rep)
+    ifelse(!dir.exists(file.path(folder_rep)), dir.create(file.path(folder_rep), recursive = TRUE), FALSE)
+    
+    #MCMC 
+    mcmc_params = mcmc_sse(sim_data, n, sigma, thinning_factor, folder_rep, rep, burn_in)
+    
+    #SAVE MCMC PARAMS 
+    saveRDS(mcmc_params, file = paste0(folder_rep, '/mcmc_params_rep_', rep, '.rds' ))
+  }
+}
 
 ##############################
+#GET_SUM_STATS_TOTAL <- function()
+  
+get_sum_stats_total <- function(base_folder_current, thinning_factor, n_reps, n_mcmc){
+  
+  'Get summary stats and p vals for all mcmc reps'
+  
+  for(rep in 1:n_reps) {
+    
+    #Get results
+    folder_rep = paste0(base_folder_current, "/rep_", rep, '/')
+    cat('rep = ', rep)
+    true_rep_sim = readRDS(paste0(folder_rep, '/sim_data.rds'))
+    mcmc_params <- readRDS(paste0(folder_rep, '/mcmc_params_rep_', rep, '.rds' ))
+    #Get true summary statistics 
+    df_true_ss = get_summary_stats(true_rep_sim, TRUE)
+    #Save 
+    saveRDS(df_true_ss, file = paste0(folder_rep, 'df_true_sum_stats_rep_', rep, '.rds' ))
+    
+    #Get parameters
+    alpha_mcmc = mcmc_params[1]; alpha_mcmc = unlist(alpha_mcmc)
+    beta_mcmc = mcmc_params[2]; beta_mcmc = unlist(beta_mcmc)
+    gamma_mcmc = mcmc_params[3]; gamma_mcmc = unlist(gamma_mcmc)
+    r0_mcmc = mcmc_params[4]; r0_mcmc = unlist(r0_mcmc)
+    
+    #Simulate data using thinned params
+    for(i in seq(burn_in, n_mcmc, by = thinning_factor)){
+      #print(paste0("mcmc summary stat rep ", i))
+      #Simulate data
+      sim_data_model_crit = simulate_branching_ss(num_days, shape_gamma, scale_gamma, alpha_mcmc[i], beta_mcmc[i], gamma_mcmc[i])
+      #Save data
+      saveRDS(sim_data_model_crit, file = paste0(folder_rep, 'mcmc/sim_data_iter_', i, '.rds' ))
+      
+      #Get summary stats. 
+      if (i == burn_in) { #first rep
+        #cat('CREATE  df_summary_stats')
+        flag_create = TRUE
+        df_summary_stats = get_summary_stats(sim_data_model_crit, flag_create)
+        flag_create = FALSE 
+        #Get indices of iterations
+        list_ss_iters = c(i)
+      } else {
+        df_summary_stats[nrow(df_summary_stats) + 1, ] = get_summary_stats(sim_data_model_crit, flag_create)
+        list_ss_iters = c(list_ss_iters, i)
+      }
+      
+    }
+    
+    #Save summary stats
+    saveRDS(df_summary_stats, file = paste0(folder_rep, '/df_summary_stats_', rep, ".rds"))
+    #print(paste0('df_summary_stats', df_summary_stats))
+    #Save ss iterations
+    saveRDS(list_ss_iters, file = paste0(folder_rep, '/list_ss_iters_i', rep, '.rds'))  
+    
+  }
+}
+
+
 #2B. TOTAL SUMMARY STATS 
 get_sum_stats_total <- function(base_folder_current, thinning_factor, n_reps, n_mcmc){
   
