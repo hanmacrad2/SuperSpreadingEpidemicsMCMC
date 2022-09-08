0675b2a6642d0a0cb75c37e2d514c3ffd4b523c6
hanmacrad2 <74420902+hanmacrad2@users.noreply.github.com>
2022-09-08 12:18:14 +0100

    Util Functions - log sum exp trick
    Util Functions - log sum exp trick

---
+++ R/UTIL_FUNCTIONS.R
@@ -0,0 +1,34 @@
+#********************
+#UTIL FUNCTIONS
+
+#'Log-sum-exp of a vector
+#'
+#' Log-sum-exp trick applied to a vector to overcome the issue of under- or overflow.
+#'  Often arise when normalizing vectors of log probabilities
+#' 
+#' @param vectorX A vector of quantities 
+#' @return normalized exponentiated vector
+#' @export LOG_SUM_EXP
+#'
+#' @author Hannah Craddock
+#'
+#' @examples
+#'
+#' vectorX = LOG_SUM_EXP(vectorX)
+#'
+
+#LOG EXP SUM
+LOG_SUM_EXP <- function(vectorX){
+  
+  max_val = max(vectorX)
+  
+  out = max_val + log(sum(exp(vectorX - max_val)))
+  
+  return(out)
+}
+
+
+X = c(-1000, -1000, -1000)
+lse = LOG_SUM_EXP(X)
+X2 = exp(X - lse) 
+X2
