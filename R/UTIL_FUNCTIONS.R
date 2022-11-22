8a74db806f4ab6387e7b8b2a2ef24d2f7f920254
hanmacrad2 <74420902+hanmacrad2@users.noreply.github.com>
2022-11-22 12:26:56 +0000

    Update Util Functions
    Update Util Functions

--- R/UTIL_FUNCTIONS.R
+++ R/UTIL_FUNCTIONS.R
@@ -45,3 +45,41 @@ LOG_SUM_EXP <- function(vectorX){
 # lse = LOG_SUM_EXP(ll)
 # ll_norm = exp(ll - lse) 
 # ll_norm
+
+
+#MCMC FUNCTIONS
+
+#**********
+#CREDIBLE INTERVALS
+
+#MCMC COLUMNS
+get_lower_ci <- function(mcmc_col){
+  
+  lower_interval = CredibleInterval(mcmc_col, level = 0.95)[[2]]
+  
+  return(lower_interval)
+}
+
+get_upper_ci <- function(mcmc_col){
+  
+  upper_interval = CredibleInterval(mcmc_col, level = 0.95)[[3]]
+  
+  return(upper_interval)
+}
+
+get_ci_matrix <- function(mcmc_matrix){
+  
+  #Storage
+  num_cols = dim(mcmc_matrix)[2]
+  vec_lower = vector("numeric", length = num_cols)
+  vec_upper = vector("numeric", length = num_cols)
+  
+  for (i in seq(1, num_cols)){
+    
+    vec_lower[i] = get_lower_ci(mcmc_matrix[, i])
+    vec_upper[i] = get_upper_ci(mcmc_matrix[, i])
+    
+  }
+  
+  return(list(vec_lower = vec_lower, vec_upper = vec_upper))
+}
