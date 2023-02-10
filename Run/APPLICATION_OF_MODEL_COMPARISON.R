49b385e64560d17757bafa132e475cd41cc5a522
hanmacrad2 <74420902+hanmacrad2@users.noreply.github.com>
2023-02-10 18:18:14 +1100

    RJMCMC PLOT
    RJMCMC PLOT

--- Run/APPLICATION_OF_MODEL_COMPARISON.R
+++ Run/APPLICATION_OF_MODEL_COMPARISON.R
@@ -16,20 +16,21 @@ list_model_ev_sseb1 = RUN_MODEL_EV_SSEB(data_sseb1, CURRENT_OUTPUT_FOLDER)
 saveRDS(list_model_ev_sseb1, file = paste0(CURRENT_OUTPUT_FOLDER, '/list_model_ev_sseb.rds' ))
 
 #PLOT LOG EVIDENCE
+par(mfrow = c(2,1))
 plot(seq_along(list_model_ev_sseb1), list_model_ev_sseb1,
      ylim = c(min(list_model_ev_sseb1)-50, max(list_model_ev_sseb1)+50), lwd = 1, pch = 19,
-     main = 'SSEB Model - Model Evidence(log). Baseline data',
+     main = 'SSEB Model - Model Evidence(log). SSEB data',
      xlab = 'mcmc iteration', ylab = 'log(model evidence)')
 
 #PLOT 2 (Close up)
 plot(seq_along(list_model_ev_sseb1), list_model_ev_sseb1,
      ylim = c(min(list_model_ev_sseb1)-0.5, max(list_model_ev_sseb1)+0.5), lwd = 1, pch = 19,
-     main = 'SSEB Model - Model Evidence(log) (Close-up). Baseline data',
+     main = 'SSEB Model - Model Evidence(log) (Close-up). SSEB data',
      xlab = 'mcmc iteration', ylab = 'log(model evidence)')
 
 
 #***************************
-#* Bayes Factors
+#* BAYES FACTORS *TO DO
 #***************************
 log_bfs1 = list_model_ev_base[1:50] - list_model_ev_sseb1 #LOG_MODEL_EVIDENCE(mcmc_sse_output$log_like_vec)
 bayes_factors1 = exp(log_bfs1)
@@ -46,3 +47,19 @@ plot(seq_along(bayes_factors1), bayes_factors1,
      ylim = c(min(bayes_factors1)-2, max(bayes_factors1)+2), 
      main = 'Bayes Factors. Baseline vs SSEB. Baseline data',
      xlab = 'mcmc iteration', ylab = 'Bayes Factor')
+
+#***************************
+#* RJMCMC 
+#***************************
+
+#RUN RJMCMC
+n_mcmc = 50000
+start_time = Sys.time()
+print(paste0('start_time:', start_time))
+rj_sseb1 = RJMCMC_BASE_SSEB(data_sseb1, n_mcmc)
+end_time = Sys.time()
+time_elap = get_time(start_time, end_time)
+
+#PLOT (*Include in plotting function: time_elap, If SSEB ALL TRUE PARAMS!)
+true_r0 = 1.8
+PLOT_SSB_MCMC_REAL_DATA(data_sseb1, rj_sseb1, n_mcmc, true_r0)
