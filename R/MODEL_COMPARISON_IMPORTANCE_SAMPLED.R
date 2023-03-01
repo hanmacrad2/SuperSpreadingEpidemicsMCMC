e89f7881f256bc40c45e25e4f55a65bfb498f80b
hanmacrad2 <74420902+hanmacrad2@users.noreply.github.com>
2023-03-01 13:48:05 +1100

    Model evidence via importance sampling
    Model evidence via importance sampling

--- R/MODEL_COMPARISON_IMPORTANCE_SAMPLED.R
+++ R/MODEL_COMPARISON_IMPORTANCE_SAMPLED.R
@@ -181,3 +181,78 @@ GET_IMP_SAMP_MODEL_EV_SSB <-function(mcmc_samples, epidemic_data,
   
   return(p_hat_est)
 }
+
+
+#******************************************************************************
+# 1. RUN AUTOMATICALLY 
+#******************************************************************************
+RUN_MCMC_MODEL_EV_IMP_SAMP <- function(epidemic_data, OUTPUT_FOLDER, run = 1, n_repeats = 100,
+                                       FLAGS_LIST = list(FLAG_BASE = FALSE, FLAG_SSEB = FALSE,
+                                                         FLAG_SSIB = TRUE, FLAG_SSIC = FALSE)){
+  'For a given epidemic dataset and model. 
+  Get importance sampling estimate of model evidence. 
+  1. Run mcmc 2. Get estimate'
+  
+  #FOLDER
+  CURRENT_OUTPUT_FOLDER = paste0(OUTPUT_FOLDER, '/run_', run)
+  create_folder(CURRENT_OUTPUT_FOLDER)
+  
+  #Parameters
+  estimates_vec = c()
+  
+  if (FLAGS_LIST$FLAG_BASE){
+    for (i in 1:n_repeats){
+      
+      print(paste0('i = ', i))
+      #MCMC SAMPLES
+      mcmc_samples = MCMC_INFER_BASELINE(epidemic_data)
+      #SAVE MCMC
+      saveRDS(mcmc_samples, file = paste0(CURRENT_OUTPUT_FOLDER, '/mcmc_base_', i ))
+      #GET PHAT ESTIMATE OF MODEL EVIDENCE
+      phat_estimate = GET_IMP_SAMP_MODEL_EV_BASE(mcmc_samples$r0_vec, epidemic_data) 
+      estimates_vec[i] = phat_estimate
+      print(ests_base)
+    }
+  } else {
+    
+    for (i in 1:n_repeats){
+      
+      print(paste0('i = ', i))
+      
+      #MCMC SAMPLES
+      if(FLAGS_LIST$SSEB){
+        mcmc_output = MCMC_INFER_SSEB(epidemic_data)
+        saveRDS(mcmc_output, file = paste0(CURRENT_OUTPUT_FOLDER, '/mcmc_sseb_', i ))
+        mcmc_samples =  matrix(c(mcmc_output$alpha_vec, mcmc_output$beta_vec, mcmc_output$gamma_vec), ncol = 3)
+          
+      } else if (FLAGS_LIST$SSIB){
+        mcmc_output = SSI_MCMC_ADAPTIVE(epidemic_data)
+        saveRDS(mcmc_output, file = paste0(CURRENT_OUTPUT_FOLDER, '/mcmc_ssib_', i ))
+        mcmc_samples =  matrix(c(mcmc_sseb$a_vec, mcmc_sseb$b_vec, mcmc_sseb$c_vec), ncol = 3)
+        
+      } else if (FLAGS_LIST$SSIC){
+        mcmc_output = MCMC_INFER_SSIC(epidemic_data)
+        saveRDS(mcmc_output, file = paste0(CURRENT_OUTPUT_FOLDER, '/mcmc_ssic_', i )) 
+        #SSIC_PARAMS + ETA
+        #mcmc_samples = mcmc_output$
+          
+      } else if (FLAGS_LIST$SSEC) {
+        mcmc_output = SSI_MCMC_ADAPTIVE(epidemic_data)
+        saveRDS(mcmc_output, file = paste0(CURRENT_OUTPUT_FOLDER, '/mcmc_ssec_', i )) 
+      }
+      
+      #GET PHAT ESTIMATE OF MODEL EVIDENCE
+      phat_estimate = GET_IMP_SAMP_MODEL_EV_SSB(mcmc_samples, epidemic_data) 
+      estimates_vec[i] = phat_estimate
+      print(ests_base)
+    }
+    
+  }
+  
+}
+
+#FOLDER SAVE
+OUTPUT_FOLDER = "~/PhD_Warwick/Project_Epidemic_Modelling/Results/model_comparison/model_evidence/BASE_DATA/BASE"
+
+#APPLY
+RUN_MCMC_MODEL_EV_IMP_SAMP(epidemic_data, OUTPUT_FOLDER)
