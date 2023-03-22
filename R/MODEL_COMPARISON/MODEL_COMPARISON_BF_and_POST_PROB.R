7b2b9209868564d266d20e97350217ef2b0b162c
hanmacrad2 <74420902+hanmacrad2@users.noreply.github.com>
2023-03-23 10:20:14 +1100

    Model comparison - Posterior model probabilities
    Model comparison - Posterior model probabilities

--- R/MODEL_COMPARISON/MODEL_COMPARISON_BF_and_POST_PROB.R
+++ R/MODEL_COMPARISON/MODEL_COMPARISON_BF_and_POST_PROB.R
@@ -34,15 +34,15 @@ GET_LOG_BAYES_FACTORS <- function (list_log_phat_mod1, list_log_phat_mod2){
 #***********************************************************
 GET_POSTERIOR_MODEL_PROB <- function(num_models = 3, 
                                      probs_models = list(prob_mech1 = 0.25, prob_mech2 = 0.5, prob_mech3 = 0.25), #mech = mechanism; baseline (0.5) or sse (0.25)
-                                     log_phats = list(mod1 = mod1,
+                                     log_model_evidence = list(mod1 = mod1,
                                                       mod2 = mod2, mod3 = mod3)){ 
   
   #PARAMS
   vec_model_diffs = c()
   
   for(i in 1:num_models-1){
-    vec_model_diffs[i] = exp(log(probs_models[[i+1]]) + log_phats[[i+1]] -
-                               log(probs_models[[1]]) - log_phats[[1]])
+    vec_model_diffs[i] = exp(log(probs_models[[i+1]]) + log_model_evidence[[i+1]] -
+                               log(probs_models[[1]]) - log_model_evidence[[1]])
   }
   
   print(paste0('sum(vec_model_diffs) = ', sum(vec_model_diffs)))
@@ -57,21 +57,27 @@ GET_POSTERIOR_MODEL_PROB <- function(num_models = 3,
 #GET_AGGREGATE_POSTERIOR_MODEL_PROB
 GET_AGG_POSTERIOR_PROB <- function(num_models = 3, 
                                    probs_models = list(prob_mech1 = 0.25, prob_mech2 = 0.5, prob_mech3 = 0.25),
-                                   list_log_phats = list(mod1 = mod1,
+                                   FLAG_EQUAL_PROB = TRUE,
+                                   list_log_mod_evid = list(mod1 = mod1,
                                                          mod2 = mod2, mod3 = mod3)){ 
   'Get posterior probabilites for mulitple P_hat reps'
   
   #FOR EACH REP
-  num_reps = length(list_log_phats$mod1)
+  num_reps = length(list_log_mod_evid$mod1)
   vec_post_probs = c()
+  #PROBABILITY
+  if(FLAG_EQUAL_PROB){
+    probs_models = list(prob_mech1 = 1/num_models, prob_mech2 = 1/num_models, prob_mech3 = 1/num_models) 
+    print(probs_models)
+  }
   
   for (i in 1:num_reps){
     print(paste0('rep = ', i))
     vec_post_probs[i] = GET_POSTERIOR_MODEL_PROB(num_models = 3, 
                                                  probs_models = probs_models,
-                                                 log_phats = list(mod1 = list_log_phats$mod1[i],
-                                                                  mod2 = list_log_phats$mod2[i],
-                                                                  mod3 = list_log_phats$mod3[i]))
+                                                 log_model_evidence = list(mod1 = list_log_mod_evid$mod1[i],
+                                                                  mod2 = list_log_mod_evid$mod2[i],
+                                                                  mod3 = list_log_mod_evid$mod3[i]))
     
   }
   
--- versions/Run_Model_Evidence_Post_Probs_V0.R
+++ versions/Run_Model_Evidence_Post_Probs_V0.R
@@ -13,7 +13,7 @@ OUTER_FOLDER = "~/PhD_Warwick/Project_Epidemic_Modelling/Results/model_compariso
 # 1. RUN BASE MCMC
 #**********************
 list_log_ev_base = LOAD_MCMC_GET_MODEL_EV_HM(OUTER_FOLDER, FLAGS_MODELS = list(BASE = TRUE, SSEB = FALSE,
-                                                                                SSNB = FALSE, SSIC = FALSE)) 
+                                                                               SSNB = FALSE, SSIC = FALSE)) 
 #Plot RESULTS
 PLOT_MODEL_EV_RESULTS(list_log_ev_base, model_type = 'Baseline')
 mean(list_log_ev_base)
@@ -33,7 +33,7 @@ PLOT_MODEL_EV_RESULTS(list_log_ev_sseb)
 # 3. RUN SSNB MCMC
 #**********************
 list_log_ev_ssnb = LOAD_MCMC_GET_MODEL_EV_HM(OUTER_FOLDER, FLAGS_MODELS = list(BASE = FALSE, SSEB = FALSE,
-                                                                                SSNB = TRUE, SSIC = FALSE))
+                                                                               SSNB = TRUE, SSIC = FALSE))
 mean(list_log_ev_ssnb)
 sd(list_log_ev_ssnb)
 
@@ -49,16 +49,10 @@ log_bf_hm = list_log_ev_base - list_log_ev_sseb
 PLOT_BAYES_FACTORS(log_bf_hm)
 mean(log_bf_hm)
 
+
 #***********************
 # 5. POSTERIOR MODEL PROBABILITIES
 #**********************
-
-#GET_AGGREGATE_POSTERIOR_MODEL_PROB
-GET_AGG_POSTERIOR_PROB(list_log_mod_evid = list(mod1 = mod1,
-                                                            mod2 = mod2, mod3 = mod3))
-  
-vec_post_probs_hm = GET_AGG_POSTERIOR_MODEL_PROB(list_log_phats = list(mod1 = c(-101, -102, -103, 102, 101),
-                                                                     mod2 = c(-113, -114, 112, 111, -115), mod3 = c(-15, -15.5, -16, -16.1, -15.9) ))
 
 #***********************
 #* METHOD 2: IMPORTANCE SAMPLING -- MODEL EVIDENCE RESULTS
@@ -88,3 +82,21 @@ PLOT_MODEL_EV_RESULTS(lme_sseb_is)
 #PLOT TOTAL RESULTS
 BOX_PLOT_MODEL_EV_RESULTS(list_log_ev_base, lme_base_is, list_log_ev_sseb, lme_sseb_is)
 
+
+
+
+
+
+
+
+#INSPECT SINGLE SET OF RESULTS
+#Inspect MCMC results
+model_type = 'SSEB'; run_number = 1
+RESULTS_FOLDER = paste0(CURRENT_OUTPUT_FOLDER, model_type, '/run_', run_number, '/')
+
+#MCMC RESULTS
+i = 1
+mcmc_sseb_1 = readRDS(file = paste0(RESULTS_FOLDER, 'mcmc_sseb_', i))
+
+lme1 = LOG_MODEL_EVIDENCE(mcmc_sseb_1$log_like_vec)
+lme1
