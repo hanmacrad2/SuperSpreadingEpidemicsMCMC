d4d578bf3e204382a35bb9caf97a6ba6863c21aa
hanmacrad2 <74420902+hanmacrad2@users.noreply.github.com>
2023-03-23 13:42:49 +1100

    Posterior pred check plots
    Posterior pred check plots

--- inspect/GET_POSTERIOR_PREDICTIVE_VALUES_FROM_MCMC.R
+++ inspect/GET_POSTERIOR_PREDICTIVE_VALUES_FROM_MCMC.R
@@ -12,7 +12,7 @@ SAMPLE_BASELINE_MCMC <- function(mcmc_output, burn_in = burn_in){
   r0 = r0_vec[sample_index]
   posterior_pred_data = SIMULATE_BASELINE_EPIDEMIC(r0)
   #PLOT
-  lines(posterior_pred_data, col = 'orange')
+  lines(posterior_pred_data, col = 'red')
 }
 
 #SAMPLE FROM SSEB MODEL
@@ -31,11 +31,25 @@ SAMPLE_SSEB_MCMC <- function(mcmc_output, burn_in = burn_in){
   lines(posterior_pred_data, col = 'green')
 }
 
+#SAMPLE FROM SSEB MODEL
+SAMPLE_SSNB_MCMC <- function(mcmc_output, burn_in = burn_in){
+  
+  #SAMPLE
+  n_mcmc = length(mcmc_output$ssnb_params_matrix[,1])
+  sample_index = sample(burn_in:n_mcmc, 1)
+  #SAMPLE PARAMETERS
+  kX = mcmc_output$ssnb_params_matrix[sample_index, 1]; R0X = mcmc_output$ssnb_params_matrix[sample_index, 2]
+  #POSTERIOR PRED DATA
+  posterior_pred_data = SIMULATE_EPI_SSNB(num_days = 50, R0 = R0X, k = kX)
+  #PLOT
+  lines(posterior_pred_data, col = 'blue')
+}
+
 #PLOT POSTERIOR PREDICTIVE DATA
 PLOT_POSTERIOR_PRED_EPI_DATA <- function(true_epidemic_data, OUTER_FOLDER, true_R0 = 1.6,
                                          run = 1, n_repeats = 100, n_sample_repeats = 5, burn_in = 2000,
                                 FLAGS_MODELS = list(BASE = TRUE, SSEB = TRUE,
-                                                    SSIB = FALSE, SSIC = FALSE)){
+                                                    SSNB = TRUE, SSIB = FALSE)){
   'For a given epidemic dataset and model. 
   Get importance sampling estimate of model evidence. 
   1. Run mcmc 2. Get estimate'
@@ -47,7 +61,7 @@ PLOT_POSTERIOR_PRED_EPI_DATA <- function(true_epidemic_data, OUTER_FOLDER, true_
   #PLOT TRUE
   par(mfrow = c(1,1))
   count = 0
-  data_title = bquote("Posterior Predictive data. Baseline model (orange), SSEB model (green). " ~ bold(R[0]: ~ .(true_R0)))
+  data_title = bquote("Posterior Predictive data. Baseline model (red), SSEB model (green), SSNB model (blue). " ~ bold(R[0]: ~ .(true_R0)))
   plot.ts(true_epidemic_data, xlab = 'Time', ylab = 'Daily Infections count',
           ylim = c(0,100),
           main = data_title, lwd = 2,
@@ -67,6 +81,25 @@ PLOT_POSTERIOR_PRED_EPI_DATA <- function(true_epidemic_data, OUTER_FOLDER, true_
       
       for (j in 1:n_sample_repeats){
         SAMPLE_SSEB_MCMC(mcmc_output, burn_in = burn_in)
+        count = count+ 1
+      }
+    } 
+  }
+  
+  if(FLAGS_MODELS$SSNB){
+    
+    #FOLER 
+    RESULTS_FOLDER = paste0(OUTER_FOLDER, 'SSNB/run_', run) 
+    print('ssnb')
+    for (i in 1:n_repeats){
+      
+      print(paste0('i = ', i))
+      #MCMC OUTPUT
+      print(paste0(RESULTS_FOLDER, '/mcmc_', i ))
+      mcmc_output = readRDS(file = paste0(RESULTS_FOLDER, '/mcmc_', i ,'.rds'))
+      
+      for (j in 1:n_sample_repeats){
+        SAMPLE_SSNB_MCMC(mcmc_output, burn_in = burn_in)
         count = count+ 1
       }
     } 
