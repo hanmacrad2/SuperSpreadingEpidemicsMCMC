5c4842b765243834d2144a6b25904ab557acdcb6
Hannah Craddock <74420902+hanmacrad2@users.noreply.github.com>
2023-11-24 20:34:03 +0000

    Model Plotting
    Model Plotting

--- R/MODEL_COMPARISON_POSTERIOR_PROBABILITIES.R
+++ R/MODEL_COMPARISON_POSTERIOR_PROBABILITIES.R
@@ -17,6 +17,7 @@ GET_MODELS_POSTERIOR_PROBS <- function(vec_log_mod_ev, num_models = 5,
     #PARAMS
     mod_ev_i = vec_log_mod_ev[i]; prior_mod_i = prior_probs_models[i]
     vec_model_diffs = vector('numeric', length = num_models - 1)
+    #print(paste0('i: ', i, 'mod_ev_i:', mod_ev_i))
     
     #Loop over other models
     for (j in seq_along(vec_log_mod_ev)) {
@@ -31,95 +32,6 @@ GET_MODELS_POSTERIOR_PROBS <- function(vec_log_mod_ev, num_models = 5,
     post_probs_vec[i] =  1/(1 + sum(vec_model_diffs))
   }
   
+  print(post_probs_vec)
   return(post_probs_vec)
-}
-
-# POSTERIOR MODEL PROB OF ONE MODEL
-GET_POSTERIOR_MODEL_PROB <- function(probs_models, row_model_ev){
-  
-  'Given one model evidence result per model'
-  
-  #PARAMS
-  num_models = length(row_model_ev)
-  vec_model_diffs = c() 
-  
-  for(i in 1:(num_models-1)){
-    #print(i); print(probs_models[[1]]); print(log_model_evidence[[1]])
-    
-    vec_model_diffs[i] = exp(log(probs_models[i+1]) + row_model_ev[i+1] -
-                               log(probs_models[1]) - row_model_ev[1]) #Matches formula?
-  }
-  
-  posterior_prob =  1/(1 + sum(vec_model_diffs))
-  
-  print(paste0('posterior_prob= ', posterior_prob))
-  
-  return(posterior_prob)
-}
-
-#VECTOR OF MODELS POSTERIOR PROBALITIES
-GET_MODELS_POSTERIOR_PROBS <- function(list_log_mod_ev, num_models = 5, 
-                                       probs_models = c(0.5, rep(0.5/4, 4)), FLAG_BASELINE = FALSE){
-  
-  'GET POSTERIOR MODEL PROBABILITY FOR EACH MODEL'
-  #PARAM
-  vec_post_probs = vector('numeric', length = num_models)
-  
-  #PRIOR PROBS: ADJUST TO ACCOUNT FOR 50% WEIGHT ON BASELINE MODEL
-  if(FLAG_BASELINE){
-    probs_models[1] = 0.5
-  } else {
-    probs_models[1] = 0.5/4
-    probs_models[2] = 0.5
-  }
-  print(probs_models)
-  
-  for (i in 1:num_models){
-    
-    if (i == 1){
-      FLAG_BASELINE = TRUE
-      probs_models[1] = 0.5
-    }
-    vec_post_probs[i] = GET_POSTERIOR_MODEL_PROB(probs_models, list_log_mod_ev[i])
-  }
-  
-  return(vec_post_probs)
-  
-}
-
-#GET AGGREGATE OF POSTERIOR MODEL PROBABILITIES
-GET_AGG_POSTERIOR_PROBABILITES <- function(num_models, FLAG_BASELINE = FALSE,
-                                   list_log_mod_evid){ 
-  
-  'Get posterior probabilites for mulitple P_hat reps'
-  
-  #CREATE MATRIX (ROWS ARE ITERATIONS, COLUMNS ARE MODELS)
-  num_reps = length(list_log_mod_evid[[1]])
-  matrix_model_ev = matrix(nrow = num_reps, ncol = num_models)
-  print(dim(matrix_model_ev))
-  vec_post_probs = c();
-  idx_sample = sample(1:num_reps, num_reps)
-  
-  #PROBABILITY
-  probs_models = c()
-  for (i in 1:num_models){
-    probs_models = c(probs_models, 0.5/num_models)
-    matrix_model_ev[,i] = list_log_mod_evid[[i]]
-  }
-  
-  #ADJUST PROBS_MODELS TO ACCOUNT FOR 50% WEIGHT ON BASELINE MODEL
-  if(FLAG_BASELINE){
-    probs_models[1] = 0.5
-  } else {
-    probs_models[2] = 0.5
-  }
-  print(probs_models)
-  
-  for (i in 1:num_reps){
-    print(paste0('rep = ', i))
-    j = idx_sample[i]; print(j)
-    vec_post_probs[i] = GET_POSTERIOR_MODEL_PROB(probs_models, matrix_model_ev[j,])
-  }
-  
-  return(vec_post_probs)
 }
---
+++ R/PLOT_MODEL_COMPARISON.R
@@ -0,0 +1,59 @@
+#***************************
+#PLOT MODEL EVIDENCE RESULTS
+
+#*****************************
+BAR_PLOT_POST_PROBS <- function(list_vec_results, title = '') {
+  
+  #PLOT MODEL PROBABILITIES
+  
+  #SETUP
+  title = paste0('Posterior Model Probabilities. ', title)
+  df_results <- as.data.frame(do.call(cbind, list_vec_results))
+  
+  bar_colors <- c('#FFD700', '#6BA6E9', '#FF8000', '#6AA84F', 'red') 
+  
+  barplot(t(df_results), beside = TRUE, col = bar_colors, ylim = c(0, 1),
+          names.arg = colnames(df_results), main = title,
+          xlab = 'Model', ylab = 'Posterior Probability',
+          cex.lab=1.5, cex.axis=1.6, cex.main= 1.6, 
+          cex.names = 1.6, cex.sub=1.6)
+  legend('topright', legend = colnames(df_results), fill = bar_colors) #, cex = 1.1)
+  
+}
+
+
+#PLOT TWO
+BOX_PLOT_POSTERIOR_PROBS <- function(list_vec_results, titleX = '') { 
+  
+  'PLOT POSTERIOR MODEL PROBABILITY RESULTS'
+  
+  #Set up
+  title = paste0(titleX, 'Posterior Model Probabilities. ', data_type, model_ev_method) #' data. ', #' model evidence'
+  df_results <- as.data.frame(do.call(cbind, list_vec_results))
+  print(head(df_results))
+  boxplot(df_results, main = title,
+          col = c('red', 'green', 'blue', 'orange', 'black'),
+          ylim = c(0,1),
+          cex.lab=1.3, cex.axis=1.3, cex.main=1.2, cex.sub=1.3)
+  
+  
+}
+
+#MODEL EVIDENCE
+BOX_PLOT_MODEL_EV <- function(list_vec_results,
+                              title = 'Model Evidence ',
+                              data_type = '') { 
+  
+  'PLOT MODEL EVIDENCE RESULTS'
+  #Set up
+  title = paste0(title, data_type)
+  df_results <- as.data.frame(do.call(cbind, list_vec_results))
+  print(head(df_results))
+  boxplot(df_results, main = title,
+          col = c('red', 'green', 'blue', 'orange'),
+          ylim = c((min(df_results)-0.5), (max(df_results)+0.5)),
+          cex.lab=1.3, cex.axis=1.3, cex.main=1.2, cex.sub=1.3)
+  
+  
+}
+
