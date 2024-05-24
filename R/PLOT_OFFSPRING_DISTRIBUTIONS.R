9c22bef33c259bf6adb8b827c0f97237de476641
Hannah Craddock <74420902+hanmacrad2@users.noreply.github.com>
2024-05-24 11:42:41 +0100

    OFFSPRING DISTS
    OFFSPRING DISTS

--- R/PLOT_OFFSPRING_DISTRIBUTIONS.R
+++ R/PLOT_OFFSPRING_DISTRIBUTIONS.R
@@ -3,7 +3,7 @@
 #********************************************
 PLOT_OFFSPRING_DISTRIBUTIONS <- function(list_params, FLAGS_MODELS, 
                                  MODEL_COLOR, RESULTS_FOLDER,
-                                 num_offspring = 30, 
+                                 num_offspring, 
                                  plot_width = 7.5, plot_height = 5.5,
                                  cex = 1.3, main_font = 1.85, #3.2, 
                                  axis_font = 1.6, PDF = TRUE){
@@ -13,11 +13,13 @@ PLOT_OFFSPRING_DISTRIBUTIONS <- function(list_params, FLAGS_MODELS,
   
   #PARAMS
   model = names(FLAGS_MODELS)[which(unlist(FLAGS_MODELS))]
+  print(model)
   main_title =  bquote(paste(.(model), " model - Offspring Distribution"))
   param = list_params[1]
   
   if(PDF){
-    plot_folder = paste0(RESULTS_FOLDER, '/', model, '/')
+    #plot_folder = paste0(RESULTS_FOLDER, '/', model, '/')
+    plot_folder = paste0(RESULTS_FOLDER, '/plot_num_offspring_', num_offspring, '/')
     create_folder(plot_folder)
     time_stamp = GET_CURRENT_TIME_STAMP()
     pdf_file = paste0(model, '_Offspring_dist_', param, '_', time_stamp, '.pdf')  
@@ -25,7 +27,9 @@ PLOT_OFFSPRING_DISTRIBUTIONS <- function(list_params, FLAGS_MODELS,
   }
   
   #PLOT
-  if(FLAGS_MODELS$SSIB){
+  if(FLAGS_MODELS$SSEB){
+    par(mar = c(5, 5, 5, 1)) #bottom left top right
+  } else if(FLAGS_MODELS$SSIB){
     par(mar = c(4.5, 4.5, 3, 2)) #c(5, 4.7, 4, 4.7))
   } else {
     par(mar = rep(5,4))
@@ -60,7 +64,8 @@ PLOT_OFFSPRING_DISTRIBUTIONS <- function(list_params, FLAGS_MODELS,
   }
 }
 
-#DATA
+#****************
+#GET OFFSPRING DISTRIBUTIONS
 GET_OFFSPRING_DATA <- function(list_params, FLAGS_MODELS, num_offspring){
   
   #DATA
@@ -69,6 +74,7 @@ GET_OFFSPRING_DATA <- function(list_params, FLAGS_MODELS, num_offspring){
   if(FLAGS_MODELS$Baseline){
     r0_param = list_params[1]
     Z = dpois(x, lambda = r0_param)  # Compute PMF
+    print(sum(x*Z))
      
   } else if (FLAGS_MODELS$SSE || FLAGS_MODELS$SSI){
     
@@ -76,12 +82,15 @@ GET_OFFSPRING_DATA <- function(list_params, FLAGS_MODELS, num_offspring){
     k_param = list_params[2]
     prob = k_param/(k_param + r0_param)
     Z = dnbinom(x, size = k_param, prob = prob)  
+    print(sum(x*Z))
     
   } else if (FLAGS_MODELS$SSEB){
     Z = GET_OFFSPRING_SSEB(x, list_params, num_offspring)
+    print(sum(x*Z))
     
   } else if (FLAGS_MODELS$SSIB){
     Z = GET_OFFSPRING_SSIB(x, list_params)
+    print(sum(x*Z))
   }
   
   print(sum(x*Z))
@@ -153,68 +162,6 @@ GET_OFFSPRING_SSIB <- function(x, list_params){
   
 }
 
-#CAPTION
-GET_OFFSPRING_LEGEND_CAPTION <- function(list_params, FLAGS_MODELS){
-  
-  if(FLAGS_MODELS$Baseline){
-    r0_param = list_params[1]
-    #legend_offspring = bquote(paste('Poisson(', R[0], '= )', .(r0_param))) #DIDN'T WORK!!
-    legend_offspring = expression(paste(' Z ~ Poisson(R'[0], '= 2.5)'))
-    
-  } else if (FLAGS_MODELS$SSE || FLAGS_MODELS$SSI){
-    legend_offspring = expression(paste(' Z ~ NegBin(k, ', frac('k', paste('R'[0], ' + k')), ' ) | R'[0], '= 2.5, k = 0.1'))
-    #legend_offspring = expression(paste('  NegBin(k, k/R'[0], '+ k )| R'[0], '= 2.5, k = 0.1'))
-  
-  } else if (FLAGS_MODELS$SSEB) {
-    legend_offspring = expression(atop(
-      paste('  Z ~ Poisson(', alpha * R[0]),
-      paste(' + ', beta, '* Poisson()', paste('(1 - a)R'[0]), ')')))
-    legend_offspring = expression(atop(
-      paste('  Z ~ Poisson(', alpha * R[0], '+ ...')))
-    
-  }  else if (FLAGS_MODELS$SSIB){
-    #legend_offspring = expression(paste('  Z ~ 1-p*Poisson(aR'[0], ' +  ', frac(paste('(1 - a)R'[0]), b), ' )'))
-                                       # , ', frac('k', paste('R'[0], ' + k')), ' ) | R'[0], '= 2.5, k = 0.1')) 
-    
-      legend_offspring = expression(atop(
-      paste('  Z ~ (1 - p) * Poisson(', a * R[0], ' + ', paste('(1 - a)R'[0]), '/', b, ')'),
-      paste(' + ', p, '* Poisson(', a * b * R[0], ' + ', paste('(1 - a)R'[0]), ')')))
-      #paste('| ', R[0], ' = 2.5, a = 0.5, b = 10')))
-    
-    legend_offspring_wrong = expression(paste(
-      ' Z ~ (1 - p) * Poisson(', a * R[0], ' + ', paste('(1 - a)R'[0]), '/', b, ') \n',
-      p, '* Poisson(', a * b * R[0], ' + ', paste('(1 - a)R'[0]), ') \n',
-      '| ', R[0], ' = 2.5, a = 0.5, b = 10'
-    ))
-    
-    
-    
-    }
-  
-  return(legend_offspring)
-}
-
-#LEGEND 
-GET_LEGEND_OFFSPRING <- function(list_params, MODEL_COLOR, FLAGS_MODELS,
-                            legend_location = 'topright', #alpha = 0.2,
-                            cex = 1.5, inset = 0.1){
-  
-  #PARAM
-  legend_caption = GET_OFFSPRING_LEGEND_CAPTION(list_params, FLAGS_MODELS)
-  
-  #Legend
-  legend(legend_location,
-         x.intersp = 0.05, #-0.05,#CONTROLS RELATIVE TO THE PLOT
-         legend_caption,
-         cex = cex,
-         inset = -0.02,
-         #inset = c(-inset), #CONTROLS RELATIVE TO THE MARGINS NOT PLOT!
-         col = c(MODEL_COLOR),
-         lwd = 3, #rep(3, num_conds-1), #c(rep(3, num_conds-1), 2),
-         lty = 1, # rep(1, num_conds), #c(1, 1),
-         text.font = 2.3, #1.45
-         bty = "n")
-}
 
 #SSEB
 GET_OFFSPRING_SSEB_DATA <-function(list_params = c(2.5, 0.5, 10), n_samps = 100000,
@@ -252,79 +199,82 @@ GET_OFFSPRING_SSEB_DATA <-function(list_params = c(2.5, 0.5, 10), n_samps = 1000
   
 }
 
+#**************************
+#* LEGEND 
 
-# GET_OFFSPRING_TITLE <- function(list_params, FLAGS_MODELS){
-#   
-#   #PARAMS
-#   model = names(FLAGS_MODELS)[which(unlist(FLAGS_MODELS))]
-#   
-#   if(FLAGS_MODELS$Baseline){
-#     r0_param = list_params[1]
-#     main_title =  bquote(paste(.(model), " model - Offspring Distriubtion")) #. Poisson(", R[0], ' = ', .(r0_param), ')'))
-#     #main_title =  bquote(paste(.(model), " model - Offspring Distriubtion. Poisson(", R[0], ' = ', .(r0_param), ')'))
-#     
-#   } else if (FLAGS_MODELS$SSE){
-#     
-#   }
-#   
-#   return(main_title)
-# }
+#LEGEND 
+GET_LEGEND_OFFSPRING <- function(list_params, MODEL_COLOR, FLAGS_MODELS,
+                                 legend_location = 'topright', #alpha = 0.2,
+                                 cex = 1.5, inset = -0.02){
+  
+  #PARAM
+  legend_caption = GET_OFFSPRING_LEGEND_CAPTION(list_params, FLAGS_MODELS)
+  
+  #INSET
+  if (FLAGS_MODELS$SSEB){
+    inset = -0.05
+  }
+  
+  #Legend
+  legend(legend_location,
+         x.intersp = 0.05, #-0.05,#CONTROLS RELATIVE TO THE PLOT
+         legend_caption,
+         cex = cex,
+         inset = inset,
+         #inset = c(-inset), #CONTROLS RELATIVE TO THE MARGINS NOT PLOT!
+         col = c(MODEL_COLOR),
+         lwd = 3, #rep(3, num_conds-1), #c(rep(3, num_conds-1), 2),
+         lty = 1, # rep(1, num_conds), #c(1, 1),
+         text.font = 2.3, #1.45
+         bty = "n")
+  
+  #BIOMDAL MODELS EXTRA TEXT
+  if (FLAGS_MODELS$SSEB){
+    mtext(expression("|" ~ R[0] ~ "= 2.5 " ~ alpha ~ "= 0.5" ~ beta ~ "= 10"),
+          side = 3, line = -6, cex = 1.35, adj = 0.95)
 
-# #PLOT
-# plot(seq_along(epidemic_data), epidemic_data,  type = 'l',
-#      xlab = 'Time', ylab = ylabel,
-#      main = data_title,
-#      col = MODEL_COLOR,
-#      lwd = lwd, #3.5,
-#      cex.lab=cex+0.2, cex.axis=cex, cex.sub=cex-0.2,
-#      cex.main=cex+0.3, cex.main= main_font)  
-# 
-# #PLOTS
-# R0 <- 2  
-# cex = 1.7
-# # Generate data
-# x <- 0:25  # Possible number of offspring
-# pmf <- dpois(x, lambda = R0)  # Compute PMF
-# 
-# # Plot
-# model = 'Baseline'
-# main_title =  bquote(paste(.(model), " model - Offspring Distriubtion. Poisson(", R[0], ' = 2)'))
-# 
-# # Generate data
-# x <- 0:25  # Possible number of offspring
-# pmf <- dpois(x, lambda = R0)  # Compute PMF
-# 
-# barplot(pmf, names.arg = x, 
-#         col = MODEL_COLORS[1], #"skyblue",
-#         main = main_title,
-#         xlab = "Number of Offspring",
-#         ylab = "Probability",
-#         lwd = lwd, #3.5,
-#         cex.lab=cex+0.2, cex.axis=cex, cex.sub=cex-0.2,
-# #         cex.main=cex+0.3, cex.main= main_font) 
-# v1GET_OFFSPRING_SSEB <-function(x, list_params){
-#   
-#   # Parameters
-#   r0_param = list_params[1]
-#   alpha_param = list_params[2]
-#   beta_param = list_params[3]
-#   
-#   # Simulate X1 from the Poisson distribution with parameter alpha*R0
-#   pn <- dpois(x, lambda = alpha_param*r0_param) #sample
-#   
-#   # Simulate X2 from the compound Poisson distribution
-#   inner_lambda <- r0_param*(1 - alpha_param)/beta_param
-#   ps <- beta_param*dpois(x, lambda = dpois(1, lambda = inner_lambda))
-#   
-#   # Calculate the total sum Z
-#   #Two loops!!; multiple and add terms (in a triangular way)
-#   
-#   prob0 = pn[1]*ps[1]
-#   prob1 = pn[1]*ps[2] + pn[2]*ps[1]
-#   
-#   Z <- pois1_nsei + pois2_ssei
-#   
-#   # Output the simulated total number of offspring Z
-#   return(Z)
-#   
-# }
+  } else if (FLAGS_MODELS$SSIB){
+    mtext(expression("|" ~ R[0] ~ "=" ~ 2.5 ~ ", a = 0.5, b = 10"),
+          side = 3, line = -6, cex = 1.35, adj = 0.95)
+    #mtext("| R[0] = 2.5, a = 0.5, b = 10", side = 3, line = -6, cex = 1.25, adj = 0.95)
+  }
+}
+
+#**************
+# LEGEND CAPTION
+GET_OFFSPRING_LEGEND_CAPTION <- function(list_params, FLAGS_MODELS){
+  
+  if(FLAGS_MODELS$Baseline){
+    r0_param = list_params[1]
+    #legend_offspring = bquote(paste('Poisson(', R[0], '= )', .(r0_param))) #DIDN'T WORK!!
+    legend_offspring = expression(paste(' Z ~ Poisson(R'[0], '= 2.5)'))
+    
+  } else if (FLAGS_MODELS$SSE || FLAGS_MODELS$SSI){
+    legend_offspring = expression(paste(' Z ~ NegBin(k, ', frac('k', paste('R'[0], ' + k')), ' ) | R'[0], '= 2.5, k = 0.4'))
+    #legend_offspring = expression(paste('  NegBin(k, k/R'[0], '+ k )| R'[0], '= 2.5, k = 0.1'))
+    
+  } else if (FLAGS_MODELS$SSEB) {
+    legend_offspring = expression(atop(
+      paste('  Z ~ Poisson(', alpha * R[0], ')'),
+      paste('+ Poisson(', beta, '* Poisson(R'[0] (1 - alpha)/beta ), '))'))
+    
+  }  else if (FLAGS_MODELS$SSIB){
+    legend_offspring = expression(atop(
+      paste('  Z ~ (1 - p) * Poisson(', a * R[0], ' + ', paste('(1 - a)R'[0]), '/', b, ')'),
+      paste(' + ', p, '* Poisson(', a * b * R[0], ' + ', paste('(1 - a)R'[0]), ')')))
+    
+    legend_offspring2 = expression(
+      atop(
+        paste("Z ~ (1 - p) * Poisson(", a * R[0], " + ", (1 - a) * R[0] / b, ")"),
+        paste("+ ", p, " * Poisson(", a * b * R[0], " + ", (1 - a) * R[0], ")"),
+        atop(
+          paste("| ", R[0], " = 2.5, a = 0.5, b = 10")
+        )
+      )
+    )
+
+  }
+  
+  return(legend_offspring)
+}
+
